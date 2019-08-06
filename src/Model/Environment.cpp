dc3c923df4061c90d01841374f84c914c2c8602f
SHoeks <36075931+SHoeks@users.noreply.github.com>
2019-08-06 14:39:46 +0200

    Update Environment.cpp

--- src/Model/Environment.cpp
+++ src/Model/Environment.cpp
@@ -290,7 +290,7 @@ void Environment::SetOrganicPool( ) {
 
 
  
-// }
+}
 //------------------------------------------------------------------------------
 
 void Environment::SetRespiratoryCO2Pool( ) {
